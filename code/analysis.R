a11ed9a6f1e955c2d58327374817389ca885edbb
Bruno Carturan <bcarturan@psf.ca>
2024-08-20 12:15:21 -0700

    exporting the count for the proportion of populations assessed

--- code/analysis.R
+++ code/analysis.R
@@ -700,7 +700,7 @@ for(rg in regions){
 # Produce datasets -----
 #
 
-# Total count of surveys:
+#'* Total count of surveys: *
 nuseds_surveyYear <- nuseds %>%
   group_by(Year) %>%
   summarise(count = n()) %>%
@@ -711,7 +711,7 @@ head(nuseds_surveyYear)
 write.csv(paste0(wd_data_output,"/Number_populationsAssessed_total.csv"),
           row.names = F)
 
-# Count of surveys per region > species
+#'* Count of surveys per region > species *
 nuseds_surveyYear <- nuseds %>%
   group_by(region,SPECIES,Year) %>%
   summarise(count = n()) %>%
@@ -725,19 +725,106 @@ write.csv(nuseds_surveyYear,
           row.names = F)
 
 
-# Count of surveys per species > regions
-nuseds_surveyYear <- nuseds %>%
-  group_by(SPECIES,region,Year) %>%
-  summarise(count = n()) %>%
-  arrange(SPECIES,region,Year)
+#'* Proportion population assessed *
+#'  Remove populations that do not have a cuid
+# it was decided to not attribute cuid to the POP_ID that are missing one (May 31 2024)
+# dataset <- nuseds_filled 
+cond <- !is.na(nuseds$cuid)
+dataset <- nuseds[cond,]
 
-head(nuseds_surveyYear)
-View(nuseds_surveyYear)
+#' Proportion of the total number of populations
+POP_IDs <- unique(dataset$POP_ID)
+POP_ID_totNb <- length(POP_IDs)
+POP_ID_totNb # 5928
 
-write.csv(nuseds_surveyYear,
-          paste0(wd_data_output,"/Number_populationsAssessed_species_regions.csv"),
+years <- unique(dataset$Year)
+years <- years[order(years)]
+
+POP_ID_totNb_yr <- sapply(years, function(yr){
+  cond <- dataset$Year == yr
+  POP_IDs_here <- dataset$POP_ID[cond] |> unique()
+  return(length(POP_IDs_here))
+})
+
+dataset_toWrite <- data.frame(year = years,
+                              population_nb = POP_ID_totNb_yr,
+                              population_prop = POP_ID_totNb_yr / POP_ID_totNb)
+
+write.csv(dataset_toWrite,
+          paste0(wd_data_output,"/Proportion_populationsAssessed_total.csv"),
           row.names = F)
 
+
+# per regions
+dataset_toWrite <- NULL
+for(rg in regions){
+  # rg <- regions[1]
+  cond <- dataset$region == rg
+  dataset_rg <- dataset[cond,]
+  
+  POP_ID_totNb <- length(unique(dataset_rg$POP_ID))
+  
+  POP_ID_totNb_yr <- sapply(years, function(yr){
+    cond <- dataset_rg$Year == yr
+    POP_IDs_here <- dataset_rg$POP_ID[cond] |> unique()
+    return(length(POP_IDs_here))
+  })
+  
+  datahere <- data.frame(region = rg, 
+                         year = years,
+                         population_nb = POP_ID_totNb_yr,
+                         population_prop = POP_ID_totNb_yr / POP_ID_totNb)
+  
+  if(is.null(dataset_toWrite)){
+    dataset_toWrite <- datahere
+  }else{
+    dataset_toWrite <- rbind(dataset_toWrite,datahere)
+  }
+}
+
+View(dataset_toWrite)
+
+write.csv(dataset_toWrite,
+          paste0(wd_data_output,"/Proportion_populationsAssessed_regions.csv"),
+          row.names = F)
+
+
+# per species
+dataset_toWrite <- NULL
+for(sp in species){
+  # sp <- species[1]
+  cond <- dataset$SPECIES == sp
+  dataset_sp <- dataset[cond,]
+  
+  POP_ID_totNb <- length(unique(dataset_sp$POP_ID))
+  
+  POP_ID_totNb_yr <- sapply(years, function(yr){
+    cond <- dataset_sp$Year == yr
+    POP_IDs_here <- dataset_sp$POP_ID[cond] |> unique()
+    return(length(POP_IDs_here))
+  })
+  
+  datahere <- data.frame(species = sp, 
+                         year = years,
+                         population_nb = POP_ID_totNb_yr,
+                         population_prop = POP_ID_totNb_yr / POP_ID_totNb)
+  
+  if(is.null(dataset_toWrite)){
+    dataset_toWrite <- datahere
+  }else{
+    dataset_toWrite <- rbind(dataset_toWrite,datahere)
+  }
+}
+
+View(dataset_toWrite)
+max(dataset_toWrite$population_prop)
+
+write.csv(dataset_toWrite,
+          paste0(wd_data_output,"/Proportion_populationsAssessed_species.csv"),
+          row.names = F)
+
+
+# Checks: the count with number of years and nb POP_ID are a bit different
 
 nuseds$SPECIES
 
@@ -1089,7 +1176,7 @@ sum(is.na(nuseds_filled$cuid))
 sum(is.na(nuseds_filled$cu_name_pse))
 
 #
-# Figure proportions of CUs with at least one population assessed ---------
+# Figure proportions of CUs with at least one population assessed (NOT USED) ---------
 
 # Remove populations that do not have a 
 # it was decided to not attribute cuid to the POP_ID that are missing one (May 31 2024)
