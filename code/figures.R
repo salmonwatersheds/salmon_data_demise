0d75c9c07d9d05d090418871013a07316ecdd093
Stephanie Peacock <stephanie.j.peacock@gmail.com>
2024-08-22 15:34:18 -0700

    Updated based on new output dataset field names and revised trends to 1986+ instead of 1980+

--- code/figures.R
+++ code/figures.R
@@ -41,20 +41,20 @@ if(1 == 2){
 # Figure 2: Number of populations by region/species
 ###############################################################################
 
-dat <- read.csv("data_output/Number_populationsAssessed_regions_species.csv")
+dat <- read.csv("data_output/Number_Prop_populationsAssessed_regions_species.csv")
 
 par(mfrow = c(3,3), mar = c(0.5,3,0,0), oma = c(4, 1, 2, 1))
 # Note: Supress plotting of columbia (r = 9) since there is just one population per species
 for(r in 1:8){
   dat.r <- dat[dat$region == regions[r],]
   
-  plot(dat.r$Year, dat.r$count, "n", xaxt = "n", las = 1, xlab = "", ylab = "", xlim = c(1930, 2023), bty = "l", ylim = c(0, 1.1*max(dat.r$count, na.rm = TRUE)))
+  plot(dat.r$year, dat.r$count, "n", xaxt = "n", las = 1, xlab = "", ylab = "", xlim = c(1930, 2023), bty = "l", ylim = c(0, 1.1*max(dat.r$count, na.rm = TRUE)))
   abline(v = seq(1930, 2025, 10), lty = 3, col = grey(0.8))
   abline(h = pretty(dat.r$count), lty = 3, col = grey(0.8))
   
   mtext(side = 3, line = -1.5, paste0(" ", letters[r], ") ", regions[r]), adj = 0, cex = 0.8)
   for(s in 1:5){
-    points(dat.r$Year[dat.r$SPECIES == names(colours_sp)[s]], dat.r$count[dat.r$SPECIES == species[s]], "o", cex = 0.6, col = paste0(colours_sp[s], 90), pch = 19)
+    points(dat.r$year[dat.r$species == names(colours_sp)[s]], dat.r$count[dat.r$species == species[s]], "o", cex = 0.6, col = paste0(colours_sp[s], 90), pch = 19)
   }
   if(r > 6) axis(side = 1)
   
@@ -64,10 +64,10 @@ plot(1,1,"n", bty = "n", xaxt = "n", yaxt = "n", xlab = "", ylab = "")
 legend("left", pch = 19, pt.cex = 2, col = paste0(colours_sp, 90), legend = species, bty = "n", cex = 1.5, title = "Species")
 
 ###############################################################################
-# Figure Sx: Trends in montoring since 1980
+# Figure Sx: Trends in montoring since 1986
 ###############################################################################
 
-# What is the linear trend from 1980 to present in number of streams monitored by each region and species?
+# What is the linear trend from 1986 to present in number of streams monitored by each region and species?
 trend <- data.frame(
   region = rep(regions, each = 5),
   species = rep(names(colours_sp), 9),
@@ -79,8 +79,8 @@ for(r in 1:9){
   dat.r <- dat[dat$region == regions[r],]
   
   for(s in 1:5){
-    if(species[s] %in% dat.r$SPECIES){
-      fit <- lm(dat.r$count[dat.r$SPECIES == species[s] & dat.r$Year >= 1980] ~ dat.r$Year[dat.r$SPECIES == species[s] & dat.r$Year >= 1980] )
+    if(species[s] %in% dat.r$species){
+      fit <- lm(dat.r$count[dat.r$species == species[s] & dat.r$year >= 1986] ~ dat.r$year[dat.r$species == species[s] & dat.r$year >= 1986] )
       trend$slope[which(trend$region == regions[r] & trend$species == species[s])] <- coefficients(fit)[2]
       trend$se[which(trend$region == regions[r] & trend$species == species[s])] <- summary(fit)$coefficients[2,2]
     }
